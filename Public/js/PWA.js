9adb327b40b8f478c638e6dc427106e3bf884c36
Lundberg <nicolas.lundberg03@skola.stenungsund.se>
2022-10-17 14:44:35 +0200

    allt

--- Public/js/PWA.js
+++ Public/js/PWA.js
@@ -1,3 +1,13 @@
-document.addEventListener('DOMContentLoaded', init, false);
+window.onload = init();
+
 function init() {
-    console.log('empty for now')
+    console.log("Hej");
+    if
+
+}
+
+function clickOnBlock() {
+    let kul = document.querySelector(".hej");
+    kul.style.display = "none";
+    console.log("hej");
+}
