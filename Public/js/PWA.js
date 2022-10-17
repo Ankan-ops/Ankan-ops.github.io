83e3f8bddc5014773ade48f780d754fbd334744a
Lundberg <nicolas.lundberg03@skola.stenungsund.se>
2022-10-17 14:36:04 +0200

    allting so far

--- Public/js/PWA.js
+++ Public/js/PWA.js
@@ -1,39 +1,13 @@
-//document.addEventListener('DOMContentLoaded', init, false);
-//function init() {
-//    console.log('empty for now')
-//}
+window.onload = init();
 
-let slideIndex = 1;
-showSlides(slideIndex);
+function init() {
+    console.log("Hej");
+    if
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for (i = 0; i < slides.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "");
-    }
-    slides[slideIndex - 1], style.display = block;
-    dots[slideIndex - 1].className += "active";
-
+function clickOnBlock() {
+    let kul = document.querySelector(".hej");
+    kul.style.display = "none";
+    console.log("hej");
 }
